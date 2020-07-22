19bacef5da1fb4ce1646a95eee53f900cd894404
VladimirOstojic <vladaliman@gmail.com>
2020-07-22 12:51:46 +0200

    Created the dataActions and reducer and setup the home page to work with redux

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,31 +1,23 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
-import axios from "axios";
+import PropTypes from "prop-types";
 
 import Scream from "../components/Scream";
 import Profile from "../components/Profile";
 
+import { connect } from "react-redux";
+import { getScreams } from "../redux/actions/dataActions";
+
 export class home extends Component {
-  state = {
-    screams: null,
-  };
   componentDidMount() {
-    axios
-      .get("/screams")
-      .then((res) => {
-        this.setState({
-          screams: res.data,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    this.props.getScreams();
   }
+
   render() {
-    let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map((scream) => (
-        <Scream key={scream.screamId} scream={scream} />
-      ))
+    console.log(this.props.data);
+    const { screams, loading } = this.props.data;
+    let recentScreamsMarkup = !loading ? (
+      screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
     ) : (
       <p>Loading...</p>
     );
@@ -42,4 +34,13 @@ export class home extends Component {
   }
 }
 
-export default home;
+const mapStateToProps = (state) => ({
+  data: state.data,
+});
+
+home.propTypes = {
+  getScreams: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps, { getScreams })(home);
---
+++ src/redux/actions/dataActions.js
@@ -0,0 +1,40 @@
+import {
+  SET_SCREAMS,
+  LOADING_DATA,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+} from "../types";
+import axios from "axios";
+
+export const getScreams = () => (dispatch) => {
+  dispatch({ type: LOADING_DATA });
+  axios
+    .get("/screams")
+    .then((res) => {
+      dispatch({ type: SET_SCREAMS, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: [],
+      });
+    });
+};
+
+export const likeScream = (screamId) => (dispacth) => {
+  axios
+    .get(`/scream/${screamId}/like`)
+    .then((res) => {
+      dispacth({ type: LIKE_SCREAM, payload: res.data });
+    })
+    .catch((err) => console.log(err));
+};
+
+export const unlikeScream = (screamId) => (dispacth) => {
+  axios
+    .get(`/scream/${screamId}/unlike`)
+    .then((res) => {
+      dispacth({ type: UNLIKE_SCREAM, payload: res.data });
+    })
+    .catch((err) => console.log(err));
+};
