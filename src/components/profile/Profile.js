8dbcc50a2088bc16ba5ab89fb06d9c4d8a57a483
VladimirOstojic <vladaliman@gmail.com>
2020-08-22 20:09:36 +0200

    Added the loading skeleton placeholder while the profile is geting loaded

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import MyButton from "../../util/MyButton";
 import EditDetails from "./EditDetails";
+import ProfileSkeleton from "../../util/ProfileSkeleton";
 //Mui stuff
 import Button from "@material-ui/core/Button";
 import MuiLink from "@material-ui/core/Link";
@@ -177,7 +178,7 @@ class Profile extends Component {
         </Paper>
       )
     ) : (
-      <p>loading...</p>
+      <ProfileSkeleton />
     );
 
     return profileMarkup;
